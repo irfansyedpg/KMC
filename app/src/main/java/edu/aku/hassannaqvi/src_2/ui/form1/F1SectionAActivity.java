cb42dcdb0535ce96ec5b3f5ba51f2aa7cfc43026
syed.irfanullah <irfansyed.pg@gmail.com>
2019-09-19 11:21:18 +0500

    F3 Layout XML

--- app/src/main/java/edu/aku/hassannaqvi/src_2/ui/form1/F1SectionAActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/src_2/ui/form1/F1SectionAActivity.java
@@ -1,6 +1,5 @@
 package edu.aku.hassannaqvi.src_2.ui.form1;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -51,7 +50,7 @@ public class F1SectionAActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        bi = DataBindingUtil.setContentView(this, R.layout.activity_f1_section_a);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_f3);
         bi.setCallback(this);
 
 
